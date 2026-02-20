26e457c4d207edaef48ad15b90ba90de5148d452
izzoel <ramadhani.zulfahmi@gmail.com>
2026-02-21 00:14:39 +0800

    ! Ubah isi api/index.php

--- api/index.php
+++ api/index.php
@@ -1,3 +1,9 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+use Illuminate\Http\Request;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$app->handleRequest(Request::capture());
