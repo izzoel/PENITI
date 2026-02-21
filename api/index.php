f58845df2528232e905706eeaacae5418e87dfe6
izzoel <ramadhani.zulfahmi@gmail.com>
2026-02-22 01:39:13 +0800

    ! Pengurangan size proyek

--- api/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Http\Request;
-
-require __DIR__ . '/../vendor/autoload.php';
-
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-$app->handleRequest(Request::capture());
